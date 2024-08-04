2b7b710d44d017796cdd898ee3c660f779dae066
DANCAN KARANI <karanidancan120@gmail.com>
2024-08-04 14:17:38 +0300

    my commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 
+
 const NotFoundPage = () => {
   return (
     <div className="font-sans antialiased">
@@ -11,9 +12,11 @@ const NotFoundPage = () => {
       <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-16 px-4">
         <div className="text-center">
           <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-          <p className="text-2xl font-semibold text-gray-600 mb-4">Oops! Page not found</p>
+          <p className="text-2xl font-semibold text-gray-600 mb-4">
+            Oops! Page not found
+          </p>
           <p className="text-lg text-gray-500 mb-8">
-            Sorry, the page you're looking for does not exist or has been moved.
+            Sorry, the page you&apos;re looking for does not exist or has been moved.
           </p>
           <a
             href="/"
